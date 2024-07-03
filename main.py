54b5ee8d34beea838ac9a8cec576a31921f8fd98
Vermilion0000 <gusevaroslav19@gmail.com>
2024-07-04 01:32:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+name = 'Ярослав'
+print ('Name:', name)
+age = 19
+print ('Age:', age)
+new_age = age + 1       # Надеюсь что я правильно понял формалировку задания
+print ('New age:', new_age)
+is_student = True
+print ('Is student:', is_student)
+
